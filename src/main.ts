91aa63dc8eeaeef5b7cdb85dc136011a0f8ce00d
wucunqian <2801531707@qq.com>
2024-08-09 16:13:45 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import router from './router';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-
+console.log('hello');
 createApp(App).use(router).use(createPinia()).use(ElementPlus).mount('#app');
